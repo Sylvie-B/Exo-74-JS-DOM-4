d15e8c021eae65fb0e9a4662262d471368398c77
Sylvie-B <unsolo@hotmail.fr>
2020-12-04 11:58:35 +0100

    miaou

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,11 +1,8 @@
 //Votre code ci dessous
+let cat = document.getElementById("cat-pic");
 
+function miaou (){
+    document.getElementById("cat-chat").innerHTML = "Miaou !";
+}
 
-
-
-
-
-
-
-
-
+cat.addEventListener("click", miaou);
