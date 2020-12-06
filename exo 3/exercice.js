a4d022d027712fadeda1bac9b4048de07597fca2
Sylvie-B <unsolo@hotmail.fr>
2020-12-06 16:53:14 +0100

    jeu de mots

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,14 +1,14 @@
 let libButton = document.getElementById('lib-button');
+
 let libIt = function() {
+
+    let champNoun = document.getElementById("noun").value;
+    let champAdj = document.getElementById("adjective").value;
+    let champPerson = document.getElementById("person").value;
+
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+
+    storyDiv.innerHTML = champNoun + " " + champAdj + " de " + champPerson;
 };
+
 libButton.addEventListener('click', libIt);
-
-
-
-
-
-
-
-
